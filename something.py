2d948c05caa02d22655ef68c8172278c0f8d0643
piyush-zoomtail <piyush@zoomtail.com>
2020-06-14 19:10:17 +0530

    Olaf

--- something.py
+++ something.py
@@ -6,4 +6,4 @@ import requests
 
 def ping_google():
     resp = requests.get("https://www.google.com")
-    return resp.status_code, resp.text
+    return resp.status_code
---
+++ test_something.py
@@ -0,0 +1,12 @@
+import unittest
+
+from something import ping_google
+
+
+class TestSomething(unittest.TestCase):
+    def test_ping_google_fail(self):
+        self.assertEqual(400, ping_google())
+
+
+if __name__ == "__main__":
+    unittest.main()
