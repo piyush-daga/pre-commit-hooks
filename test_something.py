860902a7c0663ed0ae931ab9a363063292fa4e02
piyush-zoomtail <piyush@zoomtail.com>
2020-06-14 19:27:41 +0530

    Checking pre-push

--- test_something.py
+++ test_something.py
@@ -5,7 +5,7 @@ from something import ping_google
 
 class TestSomething(unittest.TestCase):
     def test_ping_google_fail(self):
-        self.assertEqual(400, ping_google())
+        self.assertEqual(300, ping_google())
 
 
 if __name__ == "__main__":
