a5794721ac31dc592119ffacb1e9c892e4545cfc
piyush-zoomtail <piyush@zoomtail.com>
2020-06-14 19:29:00 +0530

    Fixed

--- test_something.py
+++ test_something.py
@@ -5,7 +5,7 @@ from something import ping_google
 
 class TestSomething(unittest.TestCase):
     def test_ping_google_fail(self):
-        self.assertEqual(300, ping_google())
+        self.assertEqual(200, ping_google())
 
 
 if __name__ == "__main__":
